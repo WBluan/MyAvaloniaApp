6658be6b3d74ef160ddf5b99ee0dc694731c1b26
luanweber <luanweber98@gmail.com>
2024-01-30 08:13:27 -0300

    test

--- MyAvaloniaApp/obj/Debug/net8.0/MyAvaloniaApp.AssemblyInfo.cs
+++ MyAvaloniaApp/obj/Debug/net8.0/MyAvaloniaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed019320727f61a3a215b9fc601a47872fd5c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
